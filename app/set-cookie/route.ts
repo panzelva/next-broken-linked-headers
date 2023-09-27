78fbf01a4322a9ce6c80df8ce9cc85694945aca5
Dan Ott <dan@dtott.com>
2023-09-27 00:19:59 -0400

    fix route

--- app/set-cookie/route.ts
+++ app/set-cookie/route.ts
@@ -1,6 +1,9 @@
 export async function GET() {
-  return new Response('<a href="/">Cookie set, test it out</a>', {
+  return new Response('<a href="/its-ok">Cookie set, test it out</a>', {
     status: 200,
-    headers: { 'Set-Cookie': `testcookie=heyo` },
+    headers: {
+      'Set-Cookie': `testcookie=heyo`,
+      'Content-Type': 'text/html; charset=utf-8',
+    },
   });
 }
